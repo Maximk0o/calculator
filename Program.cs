a7433caa4f83197e3eb2e23aeb35d217d67b7e5b
Max <max@csu.ru>
2015-04-17 11:29:08 +0500

    Create Calculator and Operation classes

--- Program.cs
+++ Program.cs
@@ -5,8 +5,34 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace calculator {
+    private struct Operation<T> {
+        String Symbol;
+        Func<T, T, T> Action;
+        uint Priority;
+
+        public Operation(String Sym, Func<T, T, T> Act, uint Prior) {
+            Symbol = Sym;
+            Action = Act;
+            Priority = Prior;
+        }
+    }
+
+    public class Calculator {
+        private List<Operation<double>> Operations;
+
+        Calculator() {
+            Operations = new List<Operation<double>>();
+
+            Operations.Add(new Operation<double>("+", (x, y) => (x + y), 10));
+            Operations.Add(new Operation<double>("-", (x, y) => (x - y), 10));
+            Operations.Add(new Operation<double>("*", (x, y) => (x * y), 20));
+            Operations.Add(new Operation<double>("/", (x, y) => (x / y), 20));
+        }
+    }
+
     class Program {
         static void Main(string[] args) {
+
         }
     }
 }
